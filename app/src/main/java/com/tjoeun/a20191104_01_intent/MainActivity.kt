1d09162a1cbf543d41f5da77df06e6ffb25314a5
ducho <ducho5358@gmail.com>
2019-11-04 19:54:37 +0900

    화면 이동 종료

--- app/src/main/java/com/tjoeun/a20191104_01_intent/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191104_01_intent/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         nameInputBtn.setOnClickListener {
 
             val intent = Intent(this, EditUserInfoActivity::class.java)
-            startActivity(intent)
+            startActivityForResult(intent, 1000)
         }
 
     }
