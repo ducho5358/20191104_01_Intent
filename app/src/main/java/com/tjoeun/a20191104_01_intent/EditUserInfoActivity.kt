d4b446f1c45d2242e298876f48a5607e2302e329
ducho <ducho5358@gmail.com>
2019-11-04 19:59:41 +0900

    확인버튼 => 입력값을 input 받아서 전송

--- app/src/main/java/com/tjoeun/a20191104_01_intent/EditUserInfoActivity.kt
+++ app/src/main/java/com/tjoeun/a20191104_01_intent/EditUserInfoActivity.kt
@@ -1,5 +1,7 @@
 package com.tjoeun.a20191104_01_intent
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_user_info.*
@@ -11,8 +13,21 @@ class EditUserInfoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_edit_user_info)
 
 
+        okBtn.setOnClickListener {
+
+            var intent = Intent()
+
+            intent.putExtra("inputName", nameEdt.text.toString())
+
+            setResult(RESULT_OK, intent)
+
+            finish()
+        }
+
         cancelBtn.setOnClickListener {
             finish()
+
+
         }
     }
 }
