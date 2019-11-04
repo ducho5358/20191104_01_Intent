8ac9f5c5a5309104ab74085eaa0d693d7f3f5507
ducho <ducho5358@gmail.com>
2019-11-04 21:26:21 +0900

    연습문제

--- app/src/main/java/com/tjoeun/a20191104_01_intent/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191104_01_intent/MainActivity.kt
@@ -4,20 +4,42 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_user_info.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
+
+    val REQ_CODE_FOR_PHONe_NUM = 1001
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+
+        phoneNameInputBtn.setOnClickListener {
+
+            val intent = Intent(this, EditPhoneNumActivity::class.java)
+            startActivityForResult(intent, REQ_CODE_FOR_PHONe_NUM)
+        }
+
         nameInputBtn.setOnClickListener {
 
             val intent = Intent(this, EditUserInfoActivity::class.java)
+
+
+            if(nameEdt.text.toString() != "이름 입력 필요") {
+                intent.putExtra()
+            }
+
             startActivityForResult(intent, 1000)
         }
+
+
 
     }
 
@@ -28,7 +50,7 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == 1000) {
             if(resultCode == Activity.RESULT_OK) {
 
-              var inputNameData = data?.getStringExtra("inputName")
+              var inputNameData = data?.getStringExtra("inputName")?.toLowerCase()
 
               inputNameData?.let {
 
@@ -37,7 +59,18 @@ class MainActivity : AppCompatActivity() {
 
               }
 
+            else if(requestCode == REQ_CODE_FOR_PHONe_NUM) {
+                    if(resultCode == Activity.RESULT_OK) {
+                        var phoneNumStr = data?.getStringExtra("inputPhoneNum")
 
+                        phoneNumStr?.let {
+                            phoneNumTxt.text = phoneNumStr
+
+                        }
+                    }
+
+
+                }
 
 
             }
